00043f1cb5d3a86b8f69f3cc7907334b08f12145
Your Name <you@example.com>
2021-05-17 17:32:08 -0400

    password hashed

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt= require('bcrypt');
 const sequelize = require('../config/connection');
+
 
 // create our User model
 class User extends Model {}
@@ -34,6 +36,16 @@ User.init(
     }
   },
   {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
