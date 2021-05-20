0b5d27c108947728a8a2917c506abc736d975557
Your Name <you@example.com>
2021-05-19 20:39:33 -0400

    vote

--- models/User.js
+++ models/User.js
@@ -1,27 +1,27 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt= require('bcrypt');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
-
 
 // create our User model
 class User extends Model {
+  // set up method to run on instance data (per user) to check password
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
-// define table columns and configuration
+// create fields/columns for User model
 User.init(
   {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     username: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     email: {
       type: DataTypes.STRING,
@@ -32,19 +32,21 @@ User.init(
       }
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [4]
-        }
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
     }
   },
   {
     hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
+
       async beforeUpdate(updatedUserData) {
         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
         return updatedUserData;
@@ -58,4 +60,4 @@ User.init(
   }
 );
 
-module.exports= User;
+module.exports = User;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,9 @@
-const router= require('express').Router();
+const router = require('express').Router();
 
-const userRoutes= require('./user-routes.js');
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
-
-
-module.exports= router;
+module.exports = router;
